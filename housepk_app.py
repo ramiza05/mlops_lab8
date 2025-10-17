6bd9629adb528b56a7e487207a276e82161ed16f
l230862 <ramiza05munir@gmail.com>
2025-10-17 19:37:45 +0500

    Edited line53

--- housepk_app.py
+++ housepk_app.py
@@ -50,7 +50,7 @@ def predict():
         field = feature_field_map[feat]
         val = request.form.get(field)
         if val is None:
-            return f"Missing value for {feat}", 400
+            a = 5
         if feat in label_encoders:
             # safe: value should be one of label_encoders[feat].classes_
             le = label_encoders[feat]
